ed6ea77bfe06ed5af872676b6e59b0c696b2d4c2
TravelerGuide <116388670+TravelerGuide@users.noreply.github.com>
2023-02-24 15:59:17 -0500

    sd

--- reviews.js
+++ reviews.js
@@ -11,32 +11,39 @@ function addReview(){
     let newTab = window.open(""); // open a new tab
     // create HTML for the new page
     let newPageHTML = `<!DOCTYPE html>
-    <head>
-  <link rel="stylesheet" href="postBrazil.css"/>
+    <!DOCTYPE html>
+<html>
+<head>
+  <title>Comments</title>
+  <link rel="stylesheet" href="postBrazil.css">
 </head>
-<div>
-<h2>Comments</h2>
-<div class="comment">
-  <h3>John Doe</h3>
-  <p class="comment-date">February 22, 2023</p>
-  <p class="comment-text">This is a great website! Keep up the good work!</p>
-</div>
-<div class="comment">
-  <h3>Jane Smith</h3>
-  <p class="comment-date">February 20, 2023</p>
-  <p class="comment-text">I love the design of this website. It's so clean and easy to use.</p>
-</div>
-<h2>Leave a Comment</h2>
-<form>
-  <label for="name">Name:</label>
-  <input type="text" id="name" name="name" required><br><br>
-  <label for="email">Email:</label>
-  <input type="email" id="email" name="email" required><br><br>
-  <label for="comment">Comment:</label><br>
-  <textarea id="comment" name="comment" required></textarea><br><br>
-  <button type="submit">Submit</button>
-</form>
-</div>
+<body>
+  <div>
+    <h2>Comments</h2>
+    <div class="comment">
+      <h3>John Doe</h3>
+      <p class="comment-date">February 22, 2023</p>
+      <p class="comment-text">This is a great website! Keep up the good work!</p>
+    </div>
+    <div class="comment">
+      <h3>Jane Smith</h3>
+      <p class="comment-date">February 20, 2023</p>
+      <p class="comment-text">I love the design of this website. It's so clean and easy to use.</p>
+    </div>
+    <h2>Leave a Comment</h2>
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required><br><br>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required><br><br>
+      <label for="comment">Comment:</label><br>
+      <textarea id="comment" name="comment" required></textarea><br><br>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</body>
+</html>
+
 
   
 `
