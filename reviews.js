92d474bfb71de9b610e6722bc2082a1f9ea9990d
TravelerGuide <116388670+TravelerGuide@users.noreply.github.com>
2023-02-24 15:47:24 -0500

    et

--- reviews.js
+++ reviews.js
@@ -11,40 +11,19 @@ function addReview(){
     let newTab = window.open(""); // open a new tab
     // create HTML for the new page
     let newPageHTML = `<!DOCTYPE html>
-<!DOCTYPE html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE-edge">
-    <meta name="viewport" content="width=device-width", initial-scale="1.0">
-    <title>Comment Box</title>
-    <link rel="stylesheet" href="postBrazil.css"/>
-</head>
-<body>
-    <div class="comment-session">
-        <div class="post-comment">
-            <div class="list">
-                <div class="user">
-                    <div class="user-meta">
-                        <div class="name">Name:</div>
-                    </div>
-                </div>
-                <div class="comment-post">Comment:</div>
-            </div>
-        </div>
-        <div class="comment-box">
-            <div class="user">
-                <div class="name">Name:</div>
-            </div>
-            <form action="" method="post">
-                <textarea name="comment" placeholder="Your Message"></textarea>
-                <button class="comment-submit">Comment</button>
-            </form>
-        </div>
-    </div>
-</body>
-</html>
+    <div>
+    <h2>Leave a Comment</h2>
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required><br><br>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required><br><br>
+      <label for="comment">Comment:</label><br>
+      <textarea id="comment" name="comment" required></textarea><br><br>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+  
 `
     newTab.document.write(newPageHTML)
 }
