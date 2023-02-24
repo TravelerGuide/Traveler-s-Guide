ad69febf90d03f8c88eafd74d0124e639b185098
TravelerGuide <116388670+TravelerGuide@users.noreply.github.com>
2023-02-24 15:38:53 -0500

    jh

--- reviews.js
+++ reviews.js
@@ -19,6 +19,7 @@ function addReview(){
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width", initial-scale="1.0">
     <title>Comment Box</title>
+    <link rel="stylesheet' href="postBrazil.css">
 </head>
 <body>
     <div class="comment-session">
