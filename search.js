3c2346f9f6cc85f32d68eeb85f2e571608ff4816
TravelerGuide <116388670+TravelerGuide@users.noreply.github.com>
2023-03-03 16:23:25 -0500

    we

--- search.js
+++ search.js
@@ -38,114 +38,115 @@ function searchForCountry(){
 <div class="w3-content" style="max-width:1100px;">
     <!-- Explore Nature -->
     <div class="w3-container">
-        <h3>${countryName}</h3>
+        <h3>Results for Brazil</h3>
     </div>
     <div class="w3-row-padding">
         <div class="w3-half w3-margin-bottom">
-            <img src="https://res.cloudinary.com/simpleview/image/upload/v1574085889/clients/norway/bryggen_wharf_bergen_hordaland_fjord_norway_photo_florian_olbrechts_34ad36ea-f7bc-4150-b48b-af2c2c14628f.jpg" alt="Norway" style="width:100%">
+            <img src="images/iguazu.jfif" alt="Norway" style="width:100%">
             <div class="w3-container w3-white">
-                <h3>Bergen, Norway</h3>
-                <p class="w3-opacity">XXX Reviews</p>
-                <p>Get to see what others have to say about Norway.</p>
+                <h3>Iguazu Falls, Foz do Iguaçu</h3>
+                <p class="w3-opacity">3 Reviews</p>
+                <p>Iguazu Waterfalls are a breathtaking natural wonder on the Argentina-Brazil border, featuring over 270 cascades that reach heights of up to 260 feet. It's a popular tourist spot and a UNESCO World Heritage Site.</p>
                 <button class="w3-button w3-margin-bottom">View Reviews</button>
             </div>
         </div>
         <div class="w3-half w3-margin-bottom">
-            <img src="https://theplanetd.com/images/places-to-visit-in-berlin-germany.jpg" alt="Germany" style="width:100%">
+            <img src="images/christredeemer.jfif" alt="Germany" style="width:100%">
             <div class="w3-container w3-white">
-                <h3>Berlin, Germany</h3>
-                <p class="w3-opacity">XXX Reviews</p>
-                <p>View itenineraries other travelers have made to help plan your own.</p>
-                <button class="w3-button w3-margin-bottom">View Itineraries</button>
-            </div>
-        </div>
-            <div class="w3-row-padding">
-        <div class="w3-half w3-margin-bottom">
-            <img src="https://res.cloudinary.com/simpleview/image/upload/v1574085889/clients/norway/bryggen_wharf_bergen_hordaland_fjord_norway_photo_florian_olbrechts_34ad36ea-f7bc-4150-b48b-af2c2c14628f.jpg" alt="Norway" style="width:100%">
-            <div class="w3-container w3-white">
-                <h3>Bergen, Norway</h3>
-                <p class="w3-opacity">XXX Reviews</p>
-                <p>Get to see what others have to say about Norway.</p>
+                <h3>Christ Redeemer, Rio de Janeiro</h3>
+                <p class="w3-opacity">2 Reviews</p>
+                <p>Christ the Redeemer is a 98-foot tall statue of Jesus Christ located on Corcovado Mountain in Rio de Janeiro, Brazil. It is an iconic symbol of Christianity and Brazilian culture.</p>
                 <button class="w3-button w3-margin-bottom">View Reviews</button>
             </div>
         </div>
-        <div class="w3-half w3-margin-bottom">
-            <img src="https://theplanetd.com/images/places-to-visit-in-berlin-germany.jpg" alt="Germany" style="width:100%">
-            <div class="w3-container w3-white">
-                <h3>Berlin, Germany</h3>
-                <p class="w3-opacity">XXX Reviews</p>
-                <p>View itenineraries other travelers have made to help plan your own.</p>
-                <button class="w3-button w3-margin-bottom">View Itineraries</button>
+        <div class="w3-row-padding">
+            <div class="w3-half w3-margin-bottom">
+                <img src="images/lencois.jfif" alt="Norway" style="width:100%">
+                <div class="w3-container w3-white">
+                    <h3>Lençóis Maranhenses National Park, Barreirinhas</h3>
+                    <p class="w3-opacity">1 Reviews</p>
+                    <p>Lençóis Maranhenses National Park is a striking landscape in Maranhão, Brazil, with miles of sand dunes interrupted by freshwater lagoons. It's a wildlife sanctuary and a popular tourist spot for hiking, swimming, and sightseeing.</p>
+                    <button class="w3-button w3-margin-bottom">View Reviews</button>
+                </div>
+            </div>
+            <div class="w3-half w3-margin-bottom">
+                <img src="images/beco.jfif" alt="Germany" style="width:100%">
+                <div class="w3-container w3-white">
+                    <h3>Beco do Batman, São Paulo</h3>
+                    <p class="w3-opacity">2 Reviews</p>
+                    <p>Beco do Batman is a colorful alleyway in São Paulo's Vila Madalena neighborhood filled with creative street art. It's a popular spot for taking photos and experiencing the city's artistic culture.</p>
+                    <button class="w3-button w3-margin-bottom">View Reviews</button>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!--Login -->
-    <div class="w3-container">
-        <div class="w3-panel w3-padding-16 w3-black w3-opacity w3-card w3-hover-opacity-off">
-            <h2>Subscribe To Us!</h2>
-            <p>Get news on new tips for traveling!.</p>
-            <label>Email</label>
-            <input class="w3-input w3-border" type="text" placeholder="Your Email">
-            <button type="button" class="w3-button w3-red w3-margin-top">Subscribe</button>
-        </div>
-    </div>
-
-
-
-    <!-- Contact -->
-    <div class="w3-container">
-        <div class="w3-panel w3-padding-16 w3-black w3-opacity w3-card w3-hover-opacity-off">
-
-            <img src="https://cdn3.vectorstock.com/i/1000x1000/10/07/letter-t-g-tour-and-travel-vector-19871007.jpg"  align="right" style="align-right" style="width:100%" height = 320 >
-
-            <h2>Contact</h2>
-            <i class="fa fa-map-marker" style="width:30px"></i> Philadelphia, US<br>
-
-            <i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515<br>
-            <i class="fa fa-envelope" style="width:30px"> </i> Email: travelersguide12@outlook.com<br>
-
-            <form action="https://formsubmit.co/travelersguide12@outlook.com" method="POST">
-                <input type="hidden" name="_next" value="https://ania-kubow.com/thankyou.html">
-
-                <p></p>
-                <input type="text" name="name"  placeholder="Message" required>
-                <p></p>
-                <input type="email" name="email"  placeholder="Email"required>
-                <p></p>
-                <button type="submit" class="w3-button w3-red w3-margin-top">Send
-            </form>
-
-
+        <!--Login -->
+        <div class="w3-container">
+            <div class="w3-panel w3-padding-16 w3-black w3-opacity w3-card w3-hover-opacity-off">
+                <h2>Subscribe To Us!</h2>
+                <p>Get news on new tips for traveling!.</p>
+                <label>Email</label>
+                <input class="w3-input w3-border" type="text" placeholder="Your Email">
+                <button type="button" class="w3-button w3-red w3-margin-top">Subscribe</button>
+            </div>
         </div>
 
 
-        <!-- End page content -->
-    </div>
+
+        <!-- Contact -->
+        <div class="w3-container">
+            <div class="w3-panel w3-padding-16 w3-black w3-opacity w3-card w3-hover-opacity-off">
+
+                <img src="https://cdn3.vectorstock.com/i/1000x1000/10/07/letter-t-g-tour-and-travel-vector-19871007.jpg"  align="right" style="align-right" style="width:100%" height = 320 >
+
+                <h2>Contact</h2>
+                <i class="fa fa-map-marker" style="width:30px"></i> Philadelphia, US<br>
+
+                <i class="fa fa-phone" style="width:30px"></i> Phone: +00 151515<br>
+                <i class="fa fa-envelope" style="width:30px"> </i> Email: travelersguide12@outlook.com<br>
+
+                <form action="https://formsubmit.co/travelersguide12@outlook.com" method="POST">
+                    <input type="hidden" name="_next" value="https://ania-kubow.com/thankyou.html">
+
+                    <p></p>
+                    <input type="text" name="name"  placeholder="Message" required>
+                    <p></p>
+                    <input type="email" name="email"  placeholder="Email"required>
+                    <p></p>
+                    <button type="submit" class="w3-button w3-red w3-margin-top">Send
+                </form>
 
 
-    <script>
-        // Tabs
-        function openLink(evt, linkName) {
-            var i, x, tablinks;
-            x = document.getElementsByClassName("myLink");
-            for (i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
+            </div>
+
+
+            <!-- End page content -->
+        </div>
+
+
+        <script>
+            // Tabs
+            function openLink(evt, linkName) {
+                var i, x, tablinks;
+                x = document.getElementsByClassName("myLink");
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+                tablinks = document.getElementsByClassName("tablink");
+                for (i = 0; i < x.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+                }
+                document.getElementById(linkName).style.display = "block";
+                evt.currentTarget.className += " w3-red";
             }
-            tablinks = document.getElementsByClassName("tablink");
-            for (i = 0; i < x.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-            }
-            document.getElementById(linkName).style.display = "block";
-            evt.currentTarget.className += " w3-red";
-        }
 
-        // Click on the first tablink on load
-        document.getElementsByClassName("tablink")[0].click();
-    </script>
+            // Click on the first tablink on load
+            document.getElementsByClassName("tablink")[0].click();
+        </script>
 
 </body>
 </html>
+
 
 `
     newTab.document.write(newPageHTML)
